9e4bd140b86536903e87b50fdc3fa1736302185c
sohaila2 <sohailahassan44@gmail.com>
2023-11-25 03:43:26 +0200

    delete files

--- lib/constants.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-const kPrimaryColor = Color(0xff2B475E);
-const kLogo = 'assets/images/chat.png';
-const kMessagesCollection = 'messages';
-const kMessage = 'message';
-const kCreatedAt = 'createdAt';
